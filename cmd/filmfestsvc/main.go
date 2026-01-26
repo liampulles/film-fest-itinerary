884bca6a24c8b147f75b79cab9984cae8cb3304c
Liam Pulles <me@liampulles.com>
2026-01-26 23:55:13 +0200

    minor improvements

--- cmd/filmfestsvc/main.go
+++ cmd/filmfestsvc/main.go
@@ -9,14 +9,13 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
 	// Setup zerolog
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	// zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
 	// Delegate to run func.
 	if err := run(); err != nil {
@@ -30,10 +29,12 @@ func run() error {
 	defer stop()
 	g, ctx := errgroup.WithContext(ctx)
 
-	// Setup and start the HTTP server.
-	serve(ctx, g, stop)
+	// Setup HTTP server
+	serve(ctx, g)
 
-	// Wait for all goroutines in the group to exit.
+	// Wait until all goroutines on the errgroup close
+	// (which will happen if they all fail to start, or
+	// the context is closed).
 	if err := g.Wait(); err != nil {
 		return err
 	}
@@ -41,7 +42,7 @@ func run() error {
 	return nil
 }
 
-func serve(ctx context.Context, g *errgroup.Group, stop context.CancelFunc) {
+func serve(ctx context.Context, g *errgroup.Group) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthz", healthHandler)
 
@@ -53,10 +54,7 @@ func serve(ctx context.Context, g *errgroup.Group, stop context.CancelFunc) {
 	// Start the HTTP server in the workgroup.
 	g.Go(func() error {
 		log.Info().Msgf("starting server on %s", srv.Addr)
-		// ListenAndServe blocks until the server is closed or an error occurs.
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			// If the server fails to start or run, close the signal context.
-			stop()
 			return fmt.Errorf("HTTP server ListenAndServe: %w", err)
 		}
 		return nil
@@ -64,14 +62,15 @@ func serve(ctx context.Context, g *errgroup.Group, stop context.CancelFunc) {
 
 	// Handle graceful shutdown in the workgroup.
 	g.Go(func() error {
-		// Wait for the context to be canceled (via signal or error in another goroutine).
+		// Wait for the context to be closed
 		<-ctx.Done()
 		log.Info().Msg("shutting down server")
 
-		// Use a separate context with a timeout for the shutdown process.
-		shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		// Give a 25 second deadline for active connections
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 25*time.Second)
 		defer cancel()
 
+		// Shutdown
 		if err := srv.Shutdown(shutdownCtx); err != nil {
 			return fmt.Errorf("server shutdown failed: %w", err)
 		}
